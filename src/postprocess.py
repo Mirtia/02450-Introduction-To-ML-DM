f63aea7a3010f13ec81f753678071485ce176dde
Myrsini Gkolemi <mirtoseri@gmail.com>
2024-03-31 15:54:03 +0200

    add postprocess (compare the performance by average)

---
+++ src/postprocess.py
@@ -0,0 +1,5 @@
+# == Comparison of the performance of the models ==
+# \text{Average Test Error for ANN} = \frac{\sum_{i=1}^{10} E^{\text{test}}_i \text{(ANN)}}{10}
+# and so on for the other models.
+
+# The one with the lowest average test error is the best model.
