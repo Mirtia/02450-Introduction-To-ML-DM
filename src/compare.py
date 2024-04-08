f07be5882eac88e707c158f2139eedba5c4d89ea
Myrsini Gkolemi <mirtoseri@gmail.com>
2024-04-08 10:57:31 +0200

    added libraries, forgot at splitting

--- src/compare.py
+++ src/compare.py
@@ -7,14 +7,15 @@
 # - ANN vs. linear regression
 # - ANN vs. baseline
 # - linear regression vs. baseline
-
+import pandas as pd
+from scipy import stats
 # We will use paired t-test
-with open("../output/results.csv", 'r') as f:
+with open("../output/target.csv", 'r') as f:
     results_df = pd.read_csv(f) 
 
 # ANN vs Ridge regression
 t_statistic, p_value = stats.ttest_rel(results_df['NN_Error'], results_df['Ridge_Error'])
-print(f"ANN vs. Ridge: t-statistic = {t_statistic}, p-value = {p_value}")
+print(f"Ridge vs. ANN: t-statistic = {t_statistic}, p-value = {p_value}")
 
 # ANN vs Baseline
 t_statistic, p_value = stats.ttest_rel(results_df['Baseline_Error'], results_df['NN_Error'])    
